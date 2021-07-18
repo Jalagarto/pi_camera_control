ccab8d3166d4aa2ceda3484ea5335e0717089c53
Jalagarto <javilazarogarcia@gmail.com>
2021-07-18 16:25:57 +0200

    .

--- utils.py
+++ utils.py
@@ -6,9 +6,12 @@ camera = PiCamera()
 camera.rotation = 180
 
 
-"""  All functions:
+"""
 __all__ = ['datetime_now', 'multiple_pics', 'show_preview',
 'take_still_picts', 'take_video']
+
+# https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/7
+# https://picamera.readthedocs.io/en/release-1.10/recipes1.html
 """
 
 def datetime_now():
@@ -41,15 +44,16 @@ def multiple_pics(pics_path_root):
         camera.capture(join(new_dir, 'image%s.jpg' % i))
     camera.stop_preview()
 
-def take_video():
-    pass
 
-# https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/7
+def take_video(secs=10):
+    camera.resolution = (640, 480)
+    camera.start_recording('/home/pi/results/my_video.h264')
+    camera.wait_recording(secs)
+    camera.stop_recording()
 
 
 if __name__=="__main__":
     # take_still_picts()
-    pics_path_root = '/home/pi/results/images' 
-    multiple_pics(pics_path_root)
-
-    ### usage: python3 utils.py
+    # pics_path_root = '/home/pi/results/images' 
+    # multiple_pics(pics_path_root)
+    take_video(secs=10)
